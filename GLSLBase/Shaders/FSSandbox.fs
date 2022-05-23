c71162ba5b432bdfd15151441b8ee2f2b2dc95e3
dirma10 <syys0104@gmail.com>
2022-05-23 19:34:38 +0900

    8주차 1차시) 원 그려보기(내부 채워진, 내부 빈)

--- GLSLBase/Shaders/FSSandbox.fs
+++ GLSLBase/Shaders/FSSandbox.fs
@@ -6,5 +6,25 @@ in vec4 v_Color;
 
 void main()
 {
-	FragColor = v_Color;
+	/*float dis = distance(v_Color.xy, vec2(0.5, 0.5));
+	vec4 newColor = vec4(0, 0, 0, 0);
+
+	if (dis < 0.5)
+	{
+		newColor = vec4(1, 1, 1, 1);	
+	}
+	else
+	{
+		newColor = vec4(0, 0, 0, 0);
+	}
+
+	FragColor = newColor; */
+
+
+	float dis = distance(v_Color.xy, vec2(0.5, 0.5));
+
+	if (dis > 0.48 && dis < 0.5)
+		FragColor = vec4(1);
+	else
+		FragColor = vec4(0);
 }
