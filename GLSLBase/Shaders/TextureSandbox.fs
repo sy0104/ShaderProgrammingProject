afd385ef6ce2d52556c1f074fa2828a9e5718b91
dirma10 <syys0104@gmail.com>
2022-06-15 20:17:27 +0900

    13주차 1차시) Lecture6 끝

--- GLSLBase/Shaders/TextureSandbox.fs
+++ GLSLBase/Shaders/TextureSandbox.fs
@@ -18,8 +18,44 @@ vec4 Flip()
 	return returnValue;
 }
 
+vec4 P1()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+
+	newTexCoord.y = abs(2.0 * (v_TexCoord.y - 0.5));
+	returnValue = texture(u_TexSampler, newTexCoord);
+
+	return returnValue;
+}
+
+vec4 P2()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+
+	newTexCoord.x = fract(v_TexCoord.x * 3.0);
+	newTexCoord.y = newTexCoord.y / 3.0 + floor(v_TexCoord.x * 3.0) / 3.0;
+	returnValue = texture(u_TexSampler, newTexCoord);
+
+	return returnValue;
+}
+
+vec4 P3()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+
+	newTexCoord.x = fract(v_TexCoord.x * 2.0) + floor(v_TexCoord.y * 2.0) * 0.5;
+	newTexCoord.y = fract(v_TexCoord.y * 2.0);
+	returnValue = texture(u_TexSampler, newTexCoord);
+
+	return returnValue;
+}
+
 void main()
 {
-	FragColor = Flip();
+	//FragColor = Flip();
+	FragColor = P3();
 	//FragColor = vec4(1,1,1,1);
 }
